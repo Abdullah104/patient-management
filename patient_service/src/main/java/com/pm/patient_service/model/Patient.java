9f13c068e8edbc41e4633bdb600171167c24bb7d
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-01-15 11:39:36 +0300

    db

---
+++ patient_service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,14 @@
+package com.pm.patient_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public record Patient(@Id @GeneratedValue(strategy = GenerationType.AUTO) UUID id, @NotNull String name,
+                      @NotNull @Email @Column(unique = true) String email, @NotNull String address,
+                      @NotNull LocalDate dateOfBirth, @NotNull LocalDate registeredDate) {
+}
