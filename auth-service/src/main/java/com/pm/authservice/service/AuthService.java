a2932fede431e8f83a6a504fd87071a172a3ffe0
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-02-10 22:26:32 +0300

    validate token

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,11 +2,14 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDto;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AuthService {
     private final UserService userService;
@@ -24,5 +27,17 @@ public class AuthService {
                 .findByEmail(loginRequestDto.email())
                 .filter(user -> passwordEncoder.matches(loginRequestDto.password(), user.getPassword()))
                 .map(user -> jwtUtil.generateToken(user.getEmail(), user.getRole()));
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+
+            return true;
+        } catch (JwtException exception) {
+            log.info(exception.getMessage());
+
+            return false;
+        }
     }
 }
--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
@@ -17,7 +20,6 @@ public class JwtUtil {
     private final Key secretKey;
 
     public JwtUtil(@Value("${jwt.secret}") String secret) {
-        log.info("secret = {}", secret);
         var keyBytes = Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8));
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
@@ -30,5 +32,17 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey).build().parseSignedClaims(token);
+        } catch (SignatureException exception) {
+            log.info(exception.getMessage());
+            throw new JwtException("Invalid JWT signature");
+        } catch (JwtException exception) {
+            log.info(exception.getMessage());
+            throw new JwtException("Invalid JWT");
+        }
     }
 }
