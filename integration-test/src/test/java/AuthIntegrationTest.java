c207a57623ad04114412b50d374660d82b8e6f57
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-02-11 08:14:05 +0300

    auth integration tests

---
+++ integration-test/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,48 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOkWithValidToken() {
+        String loginPayload = "{\"email\": \"testuser@test.com\", \"password\": \"password123\"}";
+
+        Response response = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+
+        System.out.println(response.jsonPath().getString("token"));
+    }
+
+    @Test
+    public void shouldReturnUnauthorized() {
+        String loginPayload = "{\"email\": \"testuser@test.com\", \"password\": \"password1223\"}";
+
+        Response response = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .extract()
+                .response();
+    }
+}
